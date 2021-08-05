318a7fe1867fb7fb8e3fdf0ae7897029710979ff
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2021-08-04 23:20:39 -0600

    added smooth scroll effect in css and index.js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('script.js connected successfully');
+$(document).ready(function(){
+    // smooth scrolling to all links
+    // good alternative for browsers that don't support the css smooth scroll effect
+    $("a").click(function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 400, function(){
+     
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
