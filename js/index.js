613a0805696061a6b84fcbf93e06ad34c73f6cae
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2021-08-06 22:04:29 -0600

    added a scrollToTop btn funtion and styled it

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(()=> {
     // smooth scrolling to all links
     // good alternative for browsers that don't support the css smooth scroll effect
     $("a").click(function(event) {
@@ -17,3 +17,23 @@ $(document).ready(function(){
       }
     });
   });
+
+   // store btnTop into variable:
+   let buttonTop = document.getElementById("btnTop");
+
+   // shoe btn When user scrolls down 20px from top of the document
+   window.onscroll = function() {scroll()};
+ 
+   function scroll() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+       buttonTop.style.display = "block";
+     } else {
+       buttonTop.style.display = "none";
+     }
+   }
+ 
+   // on user click scroll to the top of the document
+   function scrollToTop() {
+     document.body.scrollTop = 0; // For Safari
+     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   }
